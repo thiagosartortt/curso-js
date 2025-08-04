dac529288eae9d838f9e7571623e40ac8457dd46
thiagosartortt <thiagosartortt@gmail.com>
2025-08-04 18:06:23 -0300

    adicionado pagina de exercícios

---
+++ js-exercicios/js/exercicios.js
@@ -0,0 +1,85 @@
+function apresentarMensagemConsole() {
+    console.log("exercicio01");
+}
+
+function apresentarMensagemConsole() {
+    alert("SuperDev 7ª Edição");
+}
+
+function exercicio2() {
+    let nome = "Thiago";
+    alert ("Nome: "Thiago);
+}
+
+function exercio2() {   
+    let idade = 36; 
+    let peso = 84;  
+    let altura = 1.72;
+    let dados = let idade + let peeso + let altura;
+    alert("Dados: " idade + peeso + altura);
+}
+
+function exercicio3() {
+    let nome = Kaelthorn
+    let raça = Elfo Sombrio
+    let skill = Sombra Corta-Luz
+    let país de origem = Narethil
+    let rota = Mid Lane
+    let personagem1 = nome + " " + raca + " " + skill + " " + paisDeOrigem + " " + rota;
+    alert("personagem 1: " + personagem1);
+}
+
+function exercicio3() {
+    let nome = Brakka Ironjaw
+    let raça = Orc
+    let skill = Investida Brutal
+    let paísDeOrigem = Montanhas de Ghrakk
+    let rota = Top Lane
+    let personagem2 = nome + " " + raca + " " + skill + " " + paisDeOrigem + " " + rota;
+    alert("personagem 2: " + personagem2);
+}
+function exercicio3() {
+    let nome = Sarya Windveil
+    let raça = Humana
+    let skill = Chuva de Flechas
+    let país de origem = Reino de Velhira
+    let rota = Bot Lane
+    let personagem3 = nome + " " + raca + " " + skill + " " + paisDeOrigem + " " + rota;
+    alert("personagem 3: " + personagem3);
+}
+
+function exercicio4() {
+    let nome = "Thiago";
+    let peso = 84;
+    let altura = 1.72;
+    let imc = peso / altura;
+    alert("imc: " imc);
+}
+
+function exercicioSoma() {
+    let numero1 = "10";
+    let numero2 = "15";
+    let resultado = numero1 + numero2;
+    alert( "resultado: " resultado);
+}
+
+function exercicioSubtracao() {
+    let numero1 = "32";
+    let numero2 = "28";
+    let resultado = numero1 - numero2;
+    alert( "resultado: " resultado);
+}
+
+function exercicioMultiplicacao() {
+    let numero1 = "65";
+    let numero2 = "3";
+    let resultado = numero1 * numero2;
+    alert( "resultado: " resultado);
+}
+
+function exercicioDivisao() {
+    let numero1 = "100";
+    let numero2 = "5";
+    let resultado = numero1 / numero2;
+    alert( "resultado: " resultado);
+}
